527d3d85f93c13d1d9885f836a636036151b83dc
rrichii <raisarahmanrichi1@gmail.com>
2025-10-23 23:55:57 -0400

    Add files via upload

---
+++ New_ShellScripts/NewScalarLepwNC.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Folder containing your input files
+input_folder="/exp/dune/data/users/rrichi/MLProject/Selection_files"
+
+# Collect all .txt files in that folder into an array
+input_files=("$input_folder"/*.txt)
+
+# Loop over each input file
+for file in "${input_files[@]}"
+do
+  echo "Running ROOT with $file"
+  root -b -q "ScalarLept_wNC.C(\"$file\")"
+  echo ""
+done
