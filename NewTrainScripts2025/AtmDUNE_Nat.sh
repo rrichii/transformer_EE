67fb6fa2512a0bb0ecd169b8c0068524b7b7252c
rrichii <raisarahmanrichi1@gmail.com>
2025-11-28 16:03:03 -0500

    Update AtmDUNE_Nat.sh

--- NewTrainScripts2025/AtmDUNE_Nat.sh
+++ NewTrainScripts2025/AtmDUNE_Nat.sh
@@ -3,7 +3,7 @@
 readarray -t Json_files < AtmDUNE_NatJson.txt
 readarray -t CSV_files < CSV.txt
 
-Json_files=( "${Json_files[@]:0:20}" )
+Json_files=( "${Json_files[@]:0:4}" )
 
 # Iterate over json files
 for json_file in "${Json_files[@]}"; do
@@ -24,18 +24,20 @@ for json_file in "${Json_files[@]}"; do
             echo "Matching JSON and CSV files found:"
             echo "JSON: $json_file"
             echo "CSV: $csv_file"
-            short_base="${json_base#Numu_CC_Train_}"
+            short_base="${json_base#Numu_CC_Train_DUNEAtmo_Natural_}"
 
             # Make output directories
             out_dir="/exp/dune/data/users/${USER}/MLProject/Training_Samples/Atmospherics_DUNE_Like/Natural_Spectra/${json_base}"
+            short_dir="/exp/dune/data/users/${USER}/MLProject/Training_Samples/Atmospherics_DUNE_Like/Natural_Spectra/${short_base}"
             mkdir -p "${out_dir}/wandb"
+            mkdir -p "${short_dir}/wandb"
             
 
             # Run training
             python3 train_wide.py \
                 --epochs 40 \
                 --d-model 256 \
-                --nhead 8 \
+                --nhead 16 \
                 --noise-scalar $scalar_list_cli \
                 --noise-vector $vector_list_cli \
                 --num-layers 6 \
@@ -46,7 +48,7 @@ for json_file in "${Json_files[@]}"; do
                 --base-config "${json_file}" \
                 --data-path "${csv_file}" \
                 --save-path "${out_dir}" \
-                --wandb-dir "${out_dir}/wandb" \
+                --wandb-dir "${short_dir}/wandb" \
                 --wandb-project GENIE-Train-Atmo-DUNE-Natural-2025 \
                 --wandb-id "$short_base" \
                 --dataframe-type polars
